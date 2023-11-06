fe8b07f207d17f88a69ffa25ecba751cf25c6ad2
Amar Chhabra <146270710+achhabra77@users.noreply.github.com>
2023-11-06 18:45:23 -0500

    type_command_data_file
    Batch file that creates data folder and files for type command exercise

---
+++ Windows_CLI_type_command/Type_Command_Data_File.bat
@@ -0,0 +1,7 @@
+mkdir Text_Files
+cd Text_Files
+echo This is the first file > File_1.txt
+echo This is the second file > File_2.txt
+echo This is the third file > File_3.txt
+echo This is the fourth file > File_4.txt
+echo This is the fifth file > File_5.txt
